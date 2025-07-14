b88eef69d6764b3a4d425243e17411bce02d2c73
A-Kshitij-S <ankurkshitijsahai22@gmail.com>
2025-07-14 08:57:47 +0530

    fix: update CLIENT_URL in environment variables and clean up import statements in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './utlis/db.js';
-import userRouter from "./routes/user.route.js"
 import dotenv from 'dotenv';
-import quizRoutes from "./routes/quiz.route.js"
+import userRouter from "./routes/user.route.js";
+import quizRoutes from "./routes/quiz.route.js";
 import attemptRoutes from "./routes/quizAttempt.route.js";
-import testSeed from "./routes/testSeed.js"
-import questionRoutes from "./routes/question.route.js"
-import weekRoutes from "./routes/week.route.js"
-import courseRoutes from "./routes/course.route.js"
+import testSeed from "./routes/testSeed.js";
+import questionRoutes from "./routes/question.route.js";
+import weekRoutes from "./routes/week.route.js";
+import courseRoutes from "./routes/course.route.js";
 import enrollmentRoutes from "./routes/enrollment.route.js";
 
 dotenv.config();
@@ -18,22 +18,15 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "quiz-project-frontend-1nw2g5baw-ankur-kshitij-sahais-projects.vercel.app", // NOT "*"
-    credentials: true,               // allow cookies/headers
-  }));
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(cookieParser());
- 
-const corsOption={
-    origin:'quiz-project-frontend-1nw2g5baw-ankur-kshitij-sahais-projects.vercel.app',
-    credentials: true
-}
-app.use(cors(corsOption))
 
-// console.log("Mounting quiz routes...");
-app.use("/api/v1/user", userRouter)
+app.use("/api/v1/user", userRouter);
 app.use("/api/v1/quiz", quizRoutes);
-app.use("/api/v1/attempt", attemptRoutes);
 app.use("/api/v1/attempt", attemptRoutes);
 app.use('/api/test', testSeed);
 app.use("/api/v1/question", questionRoutes);
@@ -41,10 +34,11 @@ app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/week", weekRoutes);
 app.use("/api/v1/enrollment", enrollmentRoutes);
 
-
-// http://localhost:3000/
+app.get("/api/health", (req, res) => {
+  res.json({ message: "Backend is working fine!" });
+});
 
 app.listen(PORT, () => {
-    connectDB()
-    console.log(`Server running on port ${PORT}`);
+  connectDB();
+  console.log(`Server running on port ${PORT}`);
 });
