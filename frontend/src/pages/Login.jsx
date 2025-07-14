814be55919283fb06180e50e78f776e7f74e58b2
A-Kshitij-S <ankurkshitijsahai22@gmail.com>
2025-07-14 14:29:59 +0530

    for testing

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
 
     const handleLogin = async (e) => {
         e.preventDefault();
-
+        console.log(input)
         try {
 
             const res = await axios.post(`${USER_API_ENDPOINT}/login`, input, {
