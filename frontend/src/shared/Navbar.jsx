dc3e5dbdb17cfb2a83b6ad9006c43307482fcd29
A-Kshitij-S <ankurkshitijsahai22@gmail.com>
2025-07-14 15:53:12 +0530

    testing

--- frontend/src/shared/Navbar.jsx
+++ frontend/src/shared/Navbar.jsx
@@ -110,7 +110,7 @@ export default function Navbar() {
                               className="text-white hover:text-[#FF004F] font-semibold"
 
                             >
-                              Logout
+                              this is logout button Logout
                             </Button>
 
                           </div>
